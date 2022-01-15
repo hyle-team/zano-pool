29ba08020f192f2e216ad21fda94e5fe512c881c
muscleman <musclesonvacation@hotmail.com>
2022-01-15 11:31:38 -0600

    first commit

---
+++ website_example/config.js
@@ -0,0 +1,19 @@
+var api = "https://testv2.smartcoinpool.net/api";
+let parentCoin = "Zano"
+
+let byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH', 'EH', 'ZH', 'YH']
+
+var email = "support@poolhost.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
